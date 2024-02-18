2ebf6c810b1f4ec5467ca74fc450f7412e3e483f
Uri Shaked <uri@urishaked.com>
2024-02-18 19:21:28 +0200

    xschem deps

--- scripts/install_xschem.sh
+++ scripts/install_xschem.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-sudo apt-get install -y make gcc
+sudo apt-get install -y make gcc libx11-6 libx11-dev libxrender1 libxrender-dev libxcb1 libx11-xcb-dev libcairo2 \
+	libcairo2-dev tcl8.6 tcl8.6-dev tk8.6 tk8.6-dev flex bison libxpm4 libxpm-dev libjpeg-dev xterm vim-gtk3 \
+	gawk tcl-tclreadline
 
 git clone -b $XSCHEM_VERSION https://github.com/StefanSchippers/xschem /tmp/xschem
 cd /tmp/xschem
