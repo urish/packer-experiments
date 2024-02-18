db80a4e4efdf05f067cd0c484b5ffbc56f727576
Uri Shaked <uri@urishaked.com>
2024-02-18 17:28:39 +0200

    klayout

--- scripts/install_klayout.sh
+++ scripts/install_klayout.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
-curl -o /tmp/klayout.deb https://www.klayout.org/downloads/Ubuntu-22/klayout_$KLAYOUT_VERSION-1_amd64.deb
-sudo apt-get install -y /tmp/klayout.deb
-rm /tmp/klayout.deb
+set -e
+
+sudo apt-get install -y git gcc g++ make qtbase5-dev qttools5-dev libqt5xmlpatterns5-dev qtmultimedia5-dev \
+  libqt5multimediawidgets5 libqt5svg5-dev ruby ruby-dev python3 python3-dev libz-dev libgit2-dev
+
+git clone -b "v$KLAYOUT_VERSION" https://github.com/KLayout/klayout /tmp/klayout
+cd /tmp/klayout
+./scripts/makedeb.sh ubuntu22
+sudo apt-get install -y ./klayout_*.deb
+cp etc/klayout.desktop ~/Desktop
+gio set ~/Desktop/klayout.desktop metadata::trusted true
+chmod a+x ~/Desktop/klayout.desktop
+rm -rf /tmp/klayout
